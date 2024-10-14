3cbf59afb1f07b64763952c177bed3a936f33db2
samir11011 <inter.samir@hollat.net>
2024-10-14 10:08:39 +0300

    extract css file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import viteRewrite from 'vite-plugin-rewrite';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      transformer: "sass",
+    },
+
+  },
     build: {
     format: 'file',  // This ensures separate folders for each page
     outDir: 'dist',       // Output directory
